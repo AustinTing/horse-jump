73d092065f1c9707d9f983cc840e74778dbbdba0
CellBody <austintingwork@gmail.com>
2018-12-05 23:51:49 +0800

    Update isLonelySpaceExist

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const _ = require('lodash')
+const r = require('ramda')
 
 const write = (() => {
   let times = 0
@@ -23,12 +24,15 @@ const initBoard = () => {
 
 const formatBoard = board => {
   let thisBoard = _.cloneDeep(board)
-  let fixBoard = ''
+  let fixBoard = '   0    1    2    3    4    5    6    7    8\n'
   for (let i = 0; i < 9; i++) {
-    let border = ''
+    let border = '  '
     for (let j = 0; j < 9; j++) {
       if (_.isNull(thisBoard[i][j])) {
         thisBoard[i][j] = ' .'
+      }
+      if (j === 0) {
+        fixBoard = fixBoard + i + ' '
       }
       let num = thisBoard[i][j]
       num = num < 10 ? ' ' + num : num
@@ -49,71 +53,139 @@ const sleep = milliseconds => {
   })
 }
 
+const isInBoard = (i, j) => i >= 0 && i <= 8 && j >= 0 && j <= 8
+
+const isValidPosition = (board, i, j) => {
+  return isInBoard(i, j) && _.isNull(board[i][j])
+}
+
+const countEmptyPosition = board => {
+  let count = 0
+  _.each(board, row => {
+    _.each(row, position => {
+      if (_.isNull(position)) {
+        count++
+      }
+    })
+  })
+  return count
+}
+
+const isLonelySpaceExist = board => {
+  if (countEmptyPosition(board) === 1) return false // last one is lonely
+  let tempBoard = initBoard()
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (!_.isNull(board[i][j])) continue
+      let validPositions = []
+      if (isValidPosition(board, i + 2, j + 1)) {
+        validPositions.push({ i: i + 2, j: j + 1 })
+      }
+      if (isValidPosition(board, i + 1, j + 2)) {
+        validPositions.push({ i: i + 1, j: j + 2 })
+      }
+      if (isValidPosition(board, i - 1, j + 2)) {
+        validPositions.push({ i: i - 1, j: j + 2 })
+      }
+      if (isValidPosition(board, i - 2, j + 1)) {
+        validPositions.push({ i: i - 2, j: j + 1 })
+      }
+      if (isValidPosition(board, i - 2, j - 1)) {
+        validPositions.push({ i: i - 2, j: j - 1 })
+      }
+      if (isValidPosition(board, i - 1, j - 2)) {
+        validPositions.push({ i: i - 1, j: j - 2 })
+      }
+      if (isValidPosition(board, i + 1, j - 2)) {
+        validPositions.push({ i: i + 1, j: j - 2 })
+      }
+      if (isValidPosition(board, i + 2, j - 1)) {
+        validPositions.push({ i: i + 2, j: j - 1 })
+      }
+      if (validPositions.length === 0) return true
+      if (validPositions.length > 1) continue
+      let otherSpace = validPositions[0]
+      if (tempBoard[i][j]) {
+        // console.log(`Find Double`)
+        // console.log(i, j, validPositions)
+        // console.log(formatBoard(board))
+        return true
+      }
+      tempBoard[otherSpace.i][otherSpace.j] = 1
+    }
+  }
+  return false
+}
+
+const isBingo = board => {
+  for (let m = 0; m < 9; m++) {
+    for (let n = 0; n < 9; n++) {
+      if (_.isNull(board[m][n])) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
 var stepChoices = []
 const jump = (() => {
+  let jumpTime = 0
   let bingoTime = 0
   return async (board, i, j, step) => {
-    if (i < 0 || i > 8 || j < 0 || j > 8) {
+    // console.log('jump', i, j, step)
+    // console.log(stepChoices.toString())
+    if (!isValidPosition(board, i, j)) {
       return
     }
-    if (board[i][j]) return
-    // await sleep(1000)
-    let prefixStep = step < 10 ? ' ' + step : step
-    board[i][j] = '\x1b[43m' + prefixStep + '\x1b[0m'
-    console.log(formatBoard(board))
+    jumpTime++
+    // await sleep(300)
+    if (jumpTime % 1000000 === 0) {
+      console.log(jumpTime)
+      let prefixStep = step < 10 ? ' ' + step : step
+      board[i][j] = '\x1b[43m' + prefixStep + '\x1b[0m'
+      console.log(formatBoard(board))
+    }
+
     board[i][j] = step
+    if (isBingo(board)) {
+      console.log(`Bingo!`)
+      bingoTime = bingoTime + 1
+      console.log(formatBoard(board))
+      write(formatBoard(board))
+    }
+    if (isLonelySpaceExist(board)) {
+      board[i][j] = null
+      return
+    }
     let nextStep = step + 1
-    let nextI
-    let nextJ
-    nextI = i + 1
-    nextJ = j + 2
     stepChoices[step] = 0
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i + 2
-    nextJ = j + 1
+    await jump(board, i + 2, j + 1, nextStep)
     stepChoices[step] = 1
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i + 2
-    nextJ = j - 1
+    await jump(board, i + 1, j + 2, nextStep)
     stepChoices[step] = 2
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i + 1
-    nextJ = j - 2
+    await jump(board, i - 1, j + 2, nextStep)
     stepChoices[step] = 3
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i - 1
-    nextJ = j - 2
+    await jump(board, i - 2, j + 1, nextStep)
     stepChoices[step] = 4
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i - 2
-    nextJ = j - 1
+    await jump(board, i - 2, j - 1, nextStep)
     stepChoices[step] = 5
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i - 2
-    nextJ = j + 1
+    await jump(board, i - 1, j - 2, nextStep)
     stepChoices[step] = 6
-    await jump(board, nextI, nextJ, nextStep)
-    nextI = i - 1
-    nextJ = j + 2
+    await jump(board, i + 1, j - 2, nextStep)
     stepChoices[step] = 7
-    await jump(board, nextI, nextJ, nextStep)
+    await jump(board, i + 2, j - 1, nextStep)
+
     // console.log(`now step: ${step}, position: ${i}${j}, no next steps`)
     for (let m = 0; m < 9; m++) {
       for (let n = 0; n < 9; n++) {
         if (_.isNull(board[m][n])) {
-          // console.log(stepChoices.toString())
-          board[i][j] = null
           stepChoices = _.initial(stepChoices)
-          // bingoTime = bingoTime + 1
-          // write(bingoTime + '\n')
+          board[i][j] = null
           return
         }
       }
     }
-    console.log(`Bingo!`)
-    bingoTime = bingoTime + 1
-    console.log(formatBoard(board))
-    write(formatBoard(board))
   }
 }
 )()
@@ -129,7 +201,8 @@ const run = async () => {
   let i = 0
   let j = 0
   let step = 0
-  jump(board, i, j, step)
+  await jump(board, i, j, step)
+  console.log('done')
 }
 
 run()
